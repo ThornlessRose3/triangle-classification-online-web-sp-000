b96ce6c20e5d268da786b2934643d9bd9d5d64af
Carrie Jones <c.jessica.jones@gmail.com>
2021-02-03 14:39:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,19 +10,21 @@ class Triangle
   end
   
   def kind
-    if self.valid?
+    if 
+    if 
       if side1 == side2 && side1 == side3 && side2 == side3
         return :equilateral
       elsif side1 == side2 || side1 == side3 || side2 == side3
         return :isosceles
       elsif side1 != side2 && side1 != side3 && side2 != side3
         return :scalene
+      else
+        
       end
     end  
   end
   
-  def valid?
-    if side1 > 0 && side2 > 0 && side3 > 0
+  def valid?side1 > 0 && side2 > 0 && side3 > 0
       if (side1 + side2) > side3 || (side1 + side3) > side2 || (side2 + side3) > side1
         true
       else  
